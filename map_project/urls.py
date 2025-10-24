87c1cdc91e3b0f07e35deb229aa845fa5bd80518
FarahAmmourah <farahamorah17@gmail.com>
2025-10-24 18:59:26 +0300

    Fix map template path and URL include

--- map_project/urls.py
+++ map_project/urls.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
 from django.urls import path, include
-from maps import views
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('select-location/', views.map_page),
+    path('select-location/', include('maps.urls')),
 ]
