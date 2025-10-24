62552749b3e5bd4fff17929f005ef74eb0b81ac7
FarahAmmourah <farahamorah17@gmail.com>
2025-10-24 18:17:06 +0300

    Fix working map route

--- map_project/urls.py
+++ map_project/urls.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 from django.urls import path, include
+from maps import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('select-location/', include('maps.urls')),
+    path('select-location/', views.map_page),
 ]
